839fbc17298585835947a5f0e91cb0a56b317613
carlos-ssh <carlos.anezeh@me.com>
2020-06-30 13:29:31 -0500

    working on JS Shopping Cart Functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Variables
-
+const carrito = document.getElementById('carrito');
+const cursos = document.getElementById('cursos');
 
 
 
@@ -8,9 +9,12 @@
 
 
 //Listeners
+cargarEventListeners();
 
-
-
+function cargarEventListeners() {
+    //this works when you click on "Add to Cart"
+    cursos.addEventListener('click', comprarCurso);
+}
 
 
 
@@ -18,3 +22,14 @@
 
 
 //Functions
+    //Function that adds the item at the Cart
+
+function comprarCurso(e){
+    e.preventDefault();
+
+    if(e.target.classList.contains('agregar-carrito')){
+        const curso = e.target.parentElement.parentElement;
+
+        console.log(curso);
+    }
+}
